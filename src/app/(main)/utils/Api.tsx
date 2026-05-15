e4754d68e62aa01fe690849fc9e5e0f5c855aa17
Sridharan B <dsri1206@gmail.com>
2026-05-15 14:20:30 +0530

    axios

--- src/app/(main)/utils/Api.tsx
+++ src/app/(main)/utils/Api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api=axios.create({
-    baseURL:process.env.NEXT_PUBLIC_API_BASE_URL
+    baseURL:process.env.NEXT_PUBLIC_API_BASE_URL||"https://nodejs-e-commerce-epry.onrender.com"
 })
 console.log("BASE URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
 api.interceptors.request.use((req)=>{
